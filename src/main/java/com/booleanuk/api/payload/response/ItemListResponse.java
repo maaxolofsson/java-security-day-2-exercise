1ff70c520cc9bf93f5f1a988de0c66ee8d1c42e6
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-17 14:27:30 +0200

    added custom reponses

---
+++ src/main/java/com/booleanuk/api/payload/response/ItemListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.api.payload.response;
+
+import com.booleanuk.api.model.Item;
+
+import java.util.List;
+
+public class ItemListResponse extends Response<List<Item>> {
+}
---
+++ src/main/java/com/booleanuk/api/payload/response/ItemResponse.java
@@ -0,0 +1,6 @@
+package com.booleanuk.api.payload.response;
+
+import com.booleanuk.api.model.Item;
+
+public class ItemResponse extends Response<Item> {
+}
