8f913564e9ed2d980e3ea5ad5f2ba04bb489ffff
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-17 14:28:00 +0200

    added item model

---
+++ src/main/java/com/booleanuk/api/model/Item.java
@@ -0,0 +1,28 @@
+package com.booleanuk.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    public Item(int id) {
+        this.id = id;
+    }
+    
+}
